8275797f77cd338c30ddb17827f26814a9f273b8
Hiroki Yokoyama <javawriter.hy@gmail.com>
2020-09-25 16:54:08 +0900

    Update run_on_nvidia_docker2.sh

--- run_on_nvidia_docker2.sh
+++ run_on_nvidia_docker2.sh
@@ -1,2 +1,2 @@
 docker run --runtime=nvidia -it --rm -p 8890:8890 --cap-add SYS_ADMIN --device /dev/fuse \
-  --security-opt apparmor=unconfined -e PASSWORD=$1 colab-local-py3
+  --security-opt apparmor=unconfined colab-local-py3
